ad6e7db9e6313e9e372b78d34e5a1a349bb16dbf
jaekkang <rkdwornjs123@gmail.com>
2023-10-14 16:36:56 +0900

    Init: Add test for check recruit method
    채용공고 서비스의 로직들이 정상 작동하는지 확인하는 테스트 추가

--- src/test/java/com/gape/recruit/service/RecruitServiceTest.java
+++ src/test/java/com/gape/recruit/service/RecruitServiceTest.java
@@ -1,4 +1,91 @@
+package com.gape.recruit.service;
+
+import com.gape.recruit.domain.Company;
+import com.gape.recruit.domain.Recruit;
+import com.gape.recruit.domain.RecruitStatus;
+import com.gape.recruit.domain.Users;
+import com.gape.recruit.repository.RecruitRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
-class RecruitServiceTest {
-  
+
+@SpringBootTest
+@Transactional
+@RunWith(SpringRunner.class)
+public class RecruitServiceTest {
+    @Autowired RecruitService recruitService;
+    @Autowired RecruitRepository recruitRepository;
+    @Autowired CompanyService companyService;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void 채용공고_추가() throws Exception {
+        // given
+        Company company = createCompany("company1", "korea", "seoul");
+
+        Long recruitId = recruitService.post(company.getId(), 1000000L, "프론트엔드 개발자 구합니다.", "Frontend", "react/typescript");
+        // when
+
+        Recruit findRecruit = recruitRepository.findOne(recruitId);
+        // then
+        assertEquals(recruitId, findRecruit.getId(), "생성된 채용공고의 id를 통해 저장소에서 불러온 데이터와 id의 값이 같아야됩니다.");
+    }
+
+    @Test
+    public void 채용공고_삭제() throws Exception {
+        // given
+        Company company = createCompany("company1", "korea", "seoul");
+        Long recruitId = recruitService.post(company.getId(), 1000000L, "프론트엔드 개발자 구합니다.", "Frontend", "react/typescript");
+
+        Recruit findRecruit = recruitService.findOne(recruitId);
+        // when
+
+        recruitService.delete(recruitId);
+
+        // then
+
+        assertEquals(RecruitStatus.INVALID, findRecruit.getStatus());
+    }
+
+    @Test
+    public void 채용공고_등록() throws Exception {
+        // given
+        Company company = createCompany("company1", "korea", "seoul");
+        Long recruitId = recruitService.post(company.getId(), 1000000L, "프론트엔드 개발자 구합니다.", "Frontend", "react/typescript");
+        Recruit findRecruit = recruitService.findOne(recruitId);
+
+        Users user = createUser("user1");
+        // when
+
+        recruitService.apply(recruitId, user.getId());
+        // then
+
+        assertTrue(findRecruit.getUsers().contains(user));
+    }
+
+    public Company createCompany(String name, String country, String region) {
+        Company company = new Company();
+        company.setName(name);
+        company.setCountry(country);
+        company.setRegion(region);
+
+        companyService.join(company);
+        return company;
+    }
+
+    public Users createUser(String name) {
+        Users user = new Users();
+        user.setName(name);
+
+        em.persist(user);
+        return user;
+    }
 }
