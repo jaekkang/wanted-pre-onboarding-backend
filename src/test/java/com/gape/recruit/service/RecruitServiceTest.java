98f1bcba3fa61ef65089d1d5f1a3c885dca9818e
jaekkang <rkdwornjs123@gmail.com>
2023-10-18 20:37:13 +0900

    Feat: Add method for RecruitService
    RecruitService의 동작들을 확인하기 위한 테스트를 추가했습니다.

--- src/test/java/com/gape/recruit/service/RecruitServiceTest.java
+++ src/test/java/com/gape/recruit/service/RecruitServiceTest.java
@@ -4,12 +4,14 @@ import com.gape.recruit.domain.Company;
 import com.gape.recruit.domain.Recruit;
 import com.gape.recruit.domain.RecruitStatus;
 import com.gape.recruit.domain.Users;
+import com.gape.recruit.dto.recruit.RecruitDto;
 import com.gape.recruit.repository.RecruitRepository;
 import jakarta.persistence.EntityManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +24,7 @@ public class RecruitServiceTest {
     @Autowired RecruitService recruitService;
     @Autowired RecruitRepository recruitRepository;
     @Autowired CompanyService companyService;
+    @Autowired UserService userService;
 
     @Autowired
     EntityManager em;
@@ -29,9 +32,9 @@ public class RecruitServiceTest {
     @Test
     public void 채용공고_추가() throws Exception {
         // given
-        Company company = createCompany("company1", "korea", "seoul");
-
-        Long recruitId = recruitService.post(company.getId(), 1000000L, "프론트엔드 개발자 구합니다.", "Frontend", "react/typescript");
+        Company company = createCompany("company3", "korea", "seoul");
+        Recruit recruit = Recruit.createRecruit(company, "Frontend",1000000L,"react/typescript", "프론트엔드 개발자 구합니다." );
+        Long recruitId = recruitService.post(recruit);
         // when
 
         Recruit findRecruit = recruitRepository.findOne(recruitId);
@@ -42,33 +45,89 @@ public class RecruitServiceTest {
     @Test
     public void 채용공고_삭제() throws Exception {
         // given
-        Company company = createCompany("company1", "korea", "seoul");
-        Long recruitId = recruitService.post(company.getId(), 1000000L, "프론트엔드 개발자 구합니다.", "Frontend", "react/typescript");
+        Company company = createCompany("company3", "korea", "seoul");
+        Recruit recruit = Recruit.createRecruit(company, "Frontend",1000000L,"react/typescript", "프론트엔드 개발자 구합니다." );
+        Long recruitId = recruitService.post(recruit);
 
         Recruit findRecruit = recruitService.findOne(recruitId);
         // when
 
-        recruitService.delete(recruitId);
+        recruitService.delete(findRecruit.getId());
 
         // then
-
-        assertEquals(RecruitStatus.INVALID, findRecruit.getStatus());
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> recruitService.findOne(recruitId), "이미 삭제된 ID로 조회시 에러가 나야됩니다.");
     }
 
     @Test
     public void 채용공고_등록() throws Exception {
         // given
-        Company company = createCompany("company1", "korea", "seoul");
-        Long recruitId = recruitService.post(company.getId(), 1000000L, "프론트엔드 개발자 구합니다.", "Frontend", "react/typescript");
+        Company company = createCompany("company3", "korea", "seoul");
+        Recruit recruit = Recruit.createRecruit(company, "Frontend",1000000L,"react/typescript", "프론트엔드 개발자 구합니다." );
+        Long recruitId = recruitService.post(recruit);
         Recruit findRecruit = recruitService.findOne(recruitId);
 
         Users user = createUser("user1");
         // when
 
-        recruitService.apply(recruitId, user.getId());
+        recruitService.apply(recruitId, user);
         // then
 
         assertTrue(findRecruit.getUsers().contains(user));
+    }
+
+    @Test
+    public void 채용공고_등록_중복사용자() throws Exception {
+        Company company = createCompany("company3", "korea", "seoul");
+        Recruit recruit = Recruit.createRecruit(company, "Frontend",1000000L,"react/typescript", "프론트엔드 개발자 구합니다." );
+        Long recruitId = recruitService.post(recruit);
+        Recruit findRecruit = recruitService.findOne(recruitId);
+
+        Users user = createUser("user1");
+        // when
+
+        recruitService.apply(recruitId, user);
+        // then
+
+        assertThrows(IllegalStateException.class, () -> recruitService.apply(recruitId, user));
+    }
+
+    @Test
+    public void 채용공고_삭제_유저등록된() throws Exception {
+        //given
+        Company company = createCompany("company3", "korea", "seoul");
+        Recruit recruit = Recruit.createRecruit(company, "Frontend",1000000L,"react/typescript", "프론트엔드 개발자 구합니다." );
+        Users user = createUser("UserC");
+        Long recruitId = recruitService.post(recruit);
+        Recruit findRecruit = recruitService.findOne(recruitId);
+        //when
+
+        recruitService.apply(recruitId, user);
+        recruitService.delete(findRecruit.getId());
+        //then
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> recruitService.findOne(recruitId), "이미 삭제된 ID로 조회시 에러가 나야됩니다.");
+    }
+
+    @Test
+    public void 채용공고_수정() {
+        //given
+        Company company = createCompany("company3", "korea", "seoul");
+        Recruit recruit = Recruit.createRecruit(company, "Frontend",1000000L,"react/typescript", "프론트엔드 개발자 구합니다." );
+        recruitService.post(recruit);
+        RecruitDto.UpdateRecruitRequest request = new RecruitDto.UpdateRecruitRequest();
+
+        request.setReward(100L);
+        request.setContent("수정된 데이터입니다.");
+        request.setPosition("수정된 포지션입니다.");
+        request.setTechStack("수정된 기술입니다.");
+        //when
+
+        recruitService.update(recruit.getId(), request);
+
+        //then
+        assertEquals(recruit.getReward(), 100L);
+        assertEquals(recruit.getContent(), "수정된 데이터입니다.");
+        assertEquals(recruit.getPosition(), "수정된 포지션입니다.");
+        assertEquals(recruit.getTechStack(), "수정된 기술입니다.");
     }
 
     public Company createCompany(String name, String country, String region) {
