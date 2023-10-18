96d0723f11c68f1739157593271886ad60c46a48
jaekkang <rkdwornjs123@gmail.com>
2023-10-18 20:04:21 +0900

    Feat: Add check logic & search logic
    데이터 유무를 확인하는 분기와, 검색을 위한 로직을 추가했습니다.

--- src/main/java/com/gape/recruit/repository/RecruitRepository.java
+++ src/main/java/com/gape/recruit/repository/RecruitRepository.java
@@ -2,10 +2,12 @@ package com.gape.recruit.repository;
 
 import com.gape.recruit.domain.Recruit;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,10 +20,33 @@ public class RecruitRepository {
     }
 
     public Recruit findOne(Long recruitId) {
-        return em.find(Recruit.class, recruitId);
+        Recruit recruit = em.find(Recruit.class, recruitId);
+        if (recruit == null)
+            throw new IllegalArgumentException("해당 ID는 존재하지 않습니다.");
+        return recruit;
+    }
+
+    public void delete(Recruit recruit) {
+        em.remove(recruit);
     }
 
     public List<Recruit> findAll() {
-        return em.createQuery("select r from Recruit", Recruit.class).getResultList();
+        return em.createQuery("select r from Recruit r", Recruit.class).getResultList();
+    }
+
+    public Optional<List<Recruit>> searchRecruit(String keyword) {
+        String jpqlQuery = "select r from Recruit r " +
+                "where r.position like :keyword " +
+                "or r.techStack like :keyword " +
+                "or r.company.name like :keyword " +
+                "or r.company.country like :keyword " +
+                "or r.company.region like :keyword";
+
+        TypedQuery<Recruit> query = em.createQuery(jpqlQuery, Recruit.class);
+        query.setParameter("keyword", "%" + keyword + "%");
+
+        List<Recruit> results = query.getResultList();
+
+        return Optional.ofNullable(results);
     }
 }
